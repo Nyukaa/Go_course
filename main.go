4a706e422cdf6f80f35599b0282e3550d851b607
Nyukaa <annashitikova@gmail.com>
2026-06-26 16:08:57 +0300

    enhance main.go with string manipulation and time formatting examples

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+// Импортируем сразу несколько пакетов с помощью круглых скобок
+import (
+	"fmt"
+	"strings"
+	"time"
+)
 
 func main() {
-    fmt.Println("Hello, Go!")
+	// 1. Работаем с текстом через пакет strings
+	message := "Welcome to Go programming!"
+	
+	upperMessage := strings.ToUpper(message)
+	lowerMessage := strings.ToLower(message)
+
+	// 2. Выводим результаты через пакет fmt
+	fmt.Println("Оригинал:   ", message)
+	fmt.Println("В верхнем:  ", upperMessage)
+	fmt.Println("В нижнем:   ", lowerMessage)
+
+	// Разделитель строк
+	fmt.Println(strings.Repeat("-", 30))
+
+	// 3. Работаем со временем через пакет time
+	currentTime := time.Now()
+	// Форматируем дату в привычный вид: День-Месяц-Год Часы:Минуты:Секунды
+	formattedTime := currentTime.Format("02-01-2006 15:04:05")
+	
+	fmt.Println("Текущее время устройства:", formattedTime)
 }
+//GOOS=windows GOARCH=amd64 go build -o myapp.exe main.go
---
+++ tasks/main_0.go
@@ -0,0 +1,35 @@
+// task1
+// package main
+
+// import "fmt"
+
+// func main() {
+//     fmt.Println("Hello, Go!")
+// }
+// task2
+// package main
+
+// import "fmt"
+
+// func main() {
+//     fmt.Println("Hello!")
+//     fmt.Println("Welcome to Go!")
+//     fmt.Println("Let's learn together!")
+// }
+// task3
+// package main
+
+// import "fmt"
+
+// func main() {
+//     fmt.Println(42)
+//     fmt.Println(3.14)
+//     fmt.Println(2 + 2)
+// }
+// package main
+
+// import "fmt"
+
+// func main() {
+//     fmt.Println(2 + 2)
+// }
